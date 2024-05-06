80e0ad3c32914e183fb5276c666c88908f6751c3
Mikael Kristiansson <mikaelkristiansson@users.noreply.github.com>
2024-05-06 15:52:12 +0200

    fix: render problems and dont exit on close button (#5)
    * chore: activate updater
    
    * fix: render problems and dont exit on close button

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,13 @@ pub(crate) mod commands;
 
 fn main() {
     tauri::Builder::default()
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![
             commands::git,
             commands::expand_scope
--- src/routes/[repositoryId]/+page.svelte
+++ src/routes/[repositoryId]/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
+	import type { PageData } from './$types';
 	import FullviewLoading from '$lib/components/FullviewLoading.svelte';
 
-	$: repositoryId = $page.params.repositoryId;
-	console.info('[ROUTE] RepositoryId:', repositoryId);
-	$: if (repositoryId) {
-		goto(`/${repositoryId}/board`, { replaceState: true });
+	export let data: PageData;
+	console.info('[ROUTE] RepositoryId:', data.repositoryId);
+	$: if (data.repositoryId) {
+		goto(`/${data.repositoryId}/board`, { replaceState: true });
 	}
 </script>
 
