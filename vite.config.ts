b4f8b5c2cae7eaf30e9bbf698ecb252c474ea628
Anton Friskopp <anton.friskopp@tradera.com>
2024-05-06 14:57:25 +0200

    upgrade to svelte 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,40 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { internalIpV4 } from 'internal-ip';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+// prep for tauri 2
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+	resolve: {
+		alias: {
+			$lib: path.resolve('./src/lib')
+		}
+	},
+
+	plugins: [sveltekit()],
+
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 5173,
+		strictPort: true,
+		host: mobile ? '0.0.0.0' : false,
+		hmr: mobile
+			? {
+					protocol: 'ws',
+					host: await internalIpV4(),
+					port: 1421
+				}
+			: undefined,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ['**/src-tauri/**']
+		}
+	}
+}));
