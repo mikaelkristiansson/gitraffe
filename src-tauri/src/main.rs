208c071a6a69d6b3e1faad13a7ba09439de8a7ab
mikaelkristiansson <mikael.kristiansson87@gmail.com>
2024-05-07 12:53:08 +0200

    fix: close on macos now hides correctly

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 pub(crate) mod commands;
 
 fn main() {
     tauri::Builder::default()
         .on_window_event(|event| match event.event() {
             tauri::WindowEvent::CloseRequested { api, .. } => {
-                event.window().hide().unwrap();
+                #[cfg(not(target_os = "macos"))]
+                {
+                    event.window().hide().unwrap();
+                }
+                #[cfg(target_os = "macos")]
+                {
+                    tauri::AppHandle::hide(&event.window().app_handle()).unwrap();
+                }
                 api.prevent_close();
             }
             _ => {}
